4db47f8f18e9a92229ce57e2d6acd630b958c790
Mharina <marinacentroafuera@gmail.com>
2024-02-08 19:28:36 +0100

    menu principal planteado

--- app/src/main/java/com/example/diney/MainActivity.kt
+++ app/src/main/java/com/example/diney/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.diney
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val boton: Button = findViewById(R.id.button)
+
+        boton.setOnClickListener{
+            val intent = Intent(this@MainActivity, MenuPrincipal::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/diney/MenuPrincipal.kt
+++ app/src/main/java/com/example/diney/MenuPrincipal.kt
@@ -2,19 +2,35 @@ package com.example.diney
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import org.imaginativeworld.whynotimagecarousel.CarouselItem
+import org.imaginativeworld.whynotimagecarousel.ImageCarousel
+import org.imaginativeworld.whynotimagecarousel.OnItemClickListener
 
 class MenuPrincipal : AppCompatActivity() {
 
-    val list = mutableListOf<CarouselItem>()
+    private val list = mutableListOf<CarouselItem>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_principal)
 
-        list.add(CarouselItem("https://pbs.twimg.com/media/EHRv5d_UUAEMUEk.jpg"))
-        list.add(CarouselItem("https://www.mundodeportivo.com/alfabeta/hero/2023/11/0x0.png?width=768&aspect_ratio=16:9&format=nowebp"))
-        list.add(CarouselItem("https://lumiere-a.akamaihd.net/v1/images/introducing-dplus-social_10646ca8.jpeg"))
+        val carousel: ImageCarousel = findViewById(R.id.carrusel)
+        list.add(CarouselItem("https://pbs.twimg.com/media/EHRv5d_UUAEMUEk.jpg", "Mandalorian"))
+        list.add(CarouselItem("https://www.mundodeportivo.com/alfabeta/hero/2023/11/0x0.png?width=768&aspect_ratio=16:9&format=nowebp", "Marvel"))
+        list.add(CarouselItem("https://lumiere-a.akamaihd.net/v1/images/introducing-dplus-social_10646ca8.jpeg", "inicio"))
 
+//        carousel.onItemClickListener = object : OnItemClickListener {
+//            override fun onClick(position: Int, carouselItem: CarouselItem) {
+//                TODO("Not yet implemented")
+//                Toast.makeText(this@MenuPrincipal, "Mandalorian: ${carouselItem.caption}", Toast.LENGTH_SHORT).show()
+//            }
+//
+//            override fun onLongClick(position: Int, dataObject: CarouselItem) {
+//                TODO("Not yet implemented")
+//            }
+//        }
+
+        carousel.addData(list)
     }
 }
